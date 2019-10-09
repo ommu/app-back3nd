f7eafeed9bed80743908f97e01130376ae908c1e
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-10-09 16:58:05 +0700

    fixed version condition

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,30 @@
-var VERSION = 'v1',
-	assetsToCache = [
-	// '/',
-];
+'use strict';
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'), 
+const version = new URL(location).searchParams.get('v');
+console.log(version);
+
+var cacheVersion = 'v1';
+console.log(cacheVersion);
+if(version)
+	cacheVersion = version;
+console.log(version);
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js'),
 
 workbox.googleAnalytics.initialize(),
 workbox.precaching.cleanupOutdatedCaches(),
+
 workbox.routing.registerRoute(
 	/(.*(?:googleapis|gstatic)\.com\/.*)|(.*\.(?:woff|woff2|ttf|eot)(\?.*)?$)/,
 	new workbox.strategies.StaleWhileRevalidate({
-		cacheName: 'google-fonts-cache-'+VERSION,
+		cacheName: 'google-fonts-cache-'+cacheVersion,
 		maxAgeSeconds: 7 * 24 * 60 * 60
 	}), "GET"),
+
 workbox.routing.registerRoute(
 	/\.js$/,
 	new workbox.strategies.CacheFirst({
-		cacheName: 'js-cache-'+VERSION,
+		cacheName: 'js-cache-'+cacheVersion,
 		plugins: [new workbox.cacheableResponse.Plugin({
 			statuses: [200],
 			headers: {
@@ -25,10 +33,11 @@ workbox.routing.registerRoute(
 			maxAgeSeconds: 7 * 24 * 60 * 60
 		})]
 	}), "GET"),
+
 workbox.routing.registerRoute(
 	/\.css$/,
 	new workbox.strategies.CacheFirst({
-		cacheName: 'css-cache-'+VERSION,
+		cacheName: 'css-cache-'+cacheVersion,
 		plugins: [new workbox.cacheableResponse.Plugin({
 			statuses: [200],
 			headers: {
@@ -38,11 +47,28 @@ workbox.routing.registerRoute(
 		})]
 	}), "GET");
 
-self.addEventListener('install', function(event) {
+// self.addEventListener('install', function(event) {
+// 	event.waitUntil(
+// 		caches.keys().then((keyList) => {
+// 			return Promise.all(
+// 				keyList
+// 				.map((key) => {
+// 					return caches.delete(key);
+// 				})
+// 			);
+// 		})
+// 	);
+// });
+
+self.addEventListener('activate', function(event) {
 	event.waitUntil(
-		caches.open(VERSION)
-		.then(function(cache) {
-			return cache.addAll(assetsToCache);
+		caches.keys().then((keyList) => {
+			return Promise.all(
+				keyList
+				.map((key) => {
+					caches.delete(key);
+				})
+			);
 		})
 	);
 });
